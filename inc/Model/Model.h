0458d5ea6f90d3f11497c2ecd9b0aaf43e30045e
duongkt <veliemkd@gmail.com>
2014-02-21 14:33:44 -0800

    Update Model.h

--- inc/Model/Model.h
+++ inc/Model/Model.h
@@ -3,6 +3,8 @@
 
 #include "ModelStructure.h"
 
-
+/* this is the postProcessing function */
+/* where the character is chosen and put into a string to be output */
+UT_string postProcessing(UT_array *)
 
 #endif
