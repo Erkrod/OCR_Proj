cd40e987c46267868cd4995ccb32a0a0c3bdf74a
duongkt <veliemkd@gmail.com>
2014-02-21 20:53:38 -0800

    woring out bugs

---
+++ src/Model/postProcessing.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utstring.h"
+#include "utarray.h"
+
+
+UT_string * postProcessing(UT_array);
+
+int main(void)
+{
+  UT_array * WholeString = NULL;
+  CharProfile NewCharProfile;
+  CharProbability NewCharProbability;
+  
+  /*initialize string profile*/
+  UT_icd StringProfile_icd = {sizeof(CharProfile), NULL, NULL, CharProfile_free};
+  utarray_new(WholeString, &StringProfile_icd);
+  
+  /*first char spot*/	
+  UT_icd CharProbability_icd = {sizeof(CharProbability), NULL, NULL, NULL};
+  utarray_new(NewCharProfile.CharChoices, &CharProbability_icd);
+  
+  /*first possibility*/
+  NewCharProbability.Char = 'a';
+  NewCharProbability.Probability = 25;
+  utarray_push_back(NewCharProfile.CharChoices, &NewCharProbability);
+  
+  
+  /*second probability*/
+  NewCharProbability.Char = 'z';
+  NewCharProbability.Probability = 75;
+  utarray_push_back(NewCharProfile.CharChoices, &NewCharProbability);
+  
+  /*push to the whole string*/
+  utarray_push_back(WholeString, &NewCharProfile);
+
+
+  /*second char spot*/	
+  utarray_new(NewCharProfile.CharChoices, &CharProbability_icd);
+  
+  /*first possibility*/
+  NewCharProbability.Char = 'A';
+  NewCharProbability.Probability = 35;
+  utarray_push_back(NewCharProfile.CharChoices, &NewCharProbability);
+  
+  
+  /*second probability*/
+  NewCharProbability.Char = 'Z';
+  NewCharProbability.Probability = 85;
+  utarray_push_back(NewCharProfile.CharChoices, &NewCharProbability);
+  
+  postProcessing(WholeString);
+  
+  return 0;
+}
+
+UT_string * postProcessing(UT_array * charList)
+{
+  UT_string * output;
+  CharProfile * currCharProfile = currCharProfile = (CharProfile *) utarray_next(charList, currCharProfile);
+  CharProbability * currCharProbability = (CharProbability *) utarray_next(currCharProfile->CharChoices, currCharProbability);
+  int count1 = 1, count2 = 1;
+  
+  int percentage = currCharProbability->Probability;
+  char chosen = currCharProbability->Char;
+  
+  
+  while((currCharProfile = (CharProfile *) utarray_next(charList, currCharProfile)))
+  {
+    printf("counter 1 = %d\n", count++);
+    count2 = 0;
+    currCharProbability = NULL;
+    
+    while((currCharProbability = (CharProbability *) utarray_next(currCharProfile->CharChoices, currCharProbability)))
+    {
+      printf("counter 2 = %d\n", count2++);
+      printf("Character is %c with probability %d\n", currCharProbability->Char, currCharProbability->Probability);
+      
+      /* found a character with a higher percentage */
+/*      if (percentage < currCharProbability->Probability)
+      {
+	 percentage = currCharProbability->Probability;
+	 chosen = currCharProbability->Char;
+      }
+*/    }
+    
+    /* adding the chosen character to the string */
+/*    utstring_new(output);
+    utstring_printf(output, chosen);
+*/  }
+/*  return output;
+*/}
