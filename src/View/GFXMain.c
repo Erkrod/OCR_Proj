8836bfb45252495f46b95ce3b81c4cafc65c2cbb
Ryan Morrison <morrisor@uci.edu>
2014-02-23 12:47:15 -0800

    ignore

--- src/View/GFXMain.c
+++ src/View/GFXMain.c
@@ -10,21 +10,21 @@
 
 int main (int   argc, char *argv[]){
  
-	ViewHandle * MainViewHandle = View_Initialize();
+  ViewHandle * MainViewHandle = View_Initialize();
 	
- gtk_init(&argc, &argv);  
+  gtk_init(&argc, &argv);  
 
- GtkWidget *window  = drawMain(MainViewHandle);  
- GtkWidget *menubar = drawMenuBar(MainViewHandle);
- GtkWidget *vbox    = gtk_vbox_new(FALSE, 0);    
-
- gtk_container_add(GTK_CONTAINER(window), vbox);        
- gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 3);
-
- /* show the window*/
- gtk_widget_show_all(window); 
-
- gtk_main(); 
-
- return 0;
+  GtkWidget *window  = drawMain(MainViewHandle);  
+  GtkWidget *menubar = drawMenuBar(MainViewHandle);
+  GtkWidget *vbox    = gtk_vbox_new(FALSE, 0);    
+  
+  gtk_container_add(GTK_CONTAINER(window), vbox);        
+  gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 3);
+  
+  /* show the window*/
+  gtk_widget_show_all(window); 
+  
+  gtk_main(); 
+  
+  return 0;
 }
