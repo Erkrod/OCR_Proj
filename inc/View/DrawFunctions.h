0db5417671460996ffe10e5394c57a00861f619c
Ryan Morrison <morrisor@uci.edu>
2014-02-22 20:57:48 -0800

    Added header file for drawing functions

---
+++ inc/View/DrawFunctions.h
@@ -0,0 +1,15 @@
+/******************************************************************/
+/* DrawFunctions.h                                                */
+/******************************************************************/
+
+#ifndef DRAWFUNCTIONS_H
+#define DRAWFUNCTIONS_H
+
+#include <gtk/gtk.h>
+#include "ClickFunctions.h"
+
+GtkWidget *drawMain();
+
+GtkWidget *drawMenuBar();
+
+#endif
