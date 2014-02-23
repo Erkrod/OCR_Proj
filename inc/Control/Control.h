ca62ae886ff08f10abcce7721a3286f9a3705e3d
Ryan Morrison <morrisor@uci.edu>
2014-02-23 12:41:28 -0800

    Migrated ClickFunctions to Control.c/Control.h

--- inc/Control/Control.h
+++ inc/Control/Control.h
@@ -30,4 +30,26 @@ void Control_CleanUp(ControlHandle *);
 /*function to take care of all event*/
 ControlHandle * Control_ProcessEvent(ObjectHandle * ClickedObject);
 
+/* functions for menu option clicking event */
+void on_open_clicked(GtkWidget *window, gpointer data);
+
+void on_save_clicked(GtkWidget *window, gpointer data);
+
+void on_convertBW_clicked(GtkWidget *window, gpointer data);
+
+void on_stainRem_clicked(GtkWidget *window, gpointer data);
+
+void on_wrinkleRem_clicked(GtkWidget *window, gpointer data);
+
+void on_rotate_clicked(GtkWidget *window, gpointer data);
+
+void on_boundaries_clicked(GtkWidget *window, gpointer data);
+
+void on_crop_clicked(GtkWidget *window, gpointer data);
+
+void on_ocr_clicked(GtkWidget *window, gpointer data);
+
+void on_edit_clicked(GtkWidget *window, gpointer data);
+
+void on_dictionary_clicked(GtkWidget *window, gpointer data);
 #endif
--- src/Control/Control.c
+++ src/Control/Control.c
@@ -23,3 +23,29 @@ ControlHandle * Control_ProcessEvent(ObjectHandle * ClickedObject){
 		printf("Put the function to open file here\n");
 	}
 }
+
+void on_open_clicked(GtkWidget *window, gpointer data){
+    
+    GtkWidget *openFile;
+       
+    openFile = gtk_file_chooser_dialog_new("Open File",
+                                           NULL,
+                                           GTK_FILE_CHOOSER_ACTION_OPEN,
+                                           GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                           GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT,
+                                           NULL);
+    
+    if (gtk_dialog_run (GTK_DIALOG (openFile)) == GTK_RESPONSE_ACCEPT)
+    {
+        char *filename;
+        
+        filename = gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (openFile));
+        /* open file function here */
+        g_free (filename);
+    }    
+    gtk_widget_destroy (openFile);    
+}
+
+void on_rotate_clicked(GtkWidget *window, gpointer data){
+
+}
