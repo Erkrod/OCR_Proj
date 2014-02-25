056a5f47fc6b9808dcd1f10455a06018634efaee
duongkt <veliemkd@gmail.com>
2014-02-25 15:03:14 -0800

    Update postProcessing.c

--- src/Model/postProcessing.c
+++ src/Model/postProcessing.c
@@ -9,6 +9,15 @@ void CharProfile_free(void * profile_in){
 	utarray_free(profile->CharChoices);
 }
 
+/* call function to populate the dictionary */
+void populateDictionary(void);
+
+/* the actual dictionary */
+UT_array * dictionary = NULL;
+
+UT_string * matchKeywords(UT_string *);
+
+/* part 1 of the post processing stage */
 UT_string * postProcessing(UT_array * charList)
 {
   UT_string * temp;
@@ -19,8 +28,8 @@ UT_string * postProcessing(UT_array * charList)
   int count = 0;
   int percentage = 0;
   char chosen = 'a';
-  char * convertChosen = "a";
   
+  populateDictionary();
   utstring_new(output);
   
   while((currCharProfile = (CharProfile *) utarray_next(charList, currCharProfile)))
@@ -47,7 +56,6 @@ UT_string * postProcessing(UT_array * charList)
       }
     }
     
-    
     /* adding the chosen character to a temp string */
     utstring_new(temp);
     utstring_printf(temp, "%c", chosen);
@@ -59,6 +67,96 @@ UT_string * postProcessing(UT_array * charList)
   /* print the string to test */
   printf("the chosen character is: %s\n", utstring_body(output));
     
-    
   return output;
+}
+
+UT_string * matchKeywords(UT_string *output)
+{
+  int i = 0, j = 0, k = 0;
+  int keyWordLen = 0;
+  int outputLen = utString_len(output);
+  char * convertedOutput = utstring_body(output);
+  char * croppedWord = "";
+  char * dictionaryWord = "";
+  int characterMatched = 0;
+  UT_string * newOutput;
+  utstring_new(newOutput);
+  
+  for (i = 0; i < 32; i++)
+  {
+	keyWordLen=sizeof(dictionary[i]);
+	dictionaryWord = dictionary[i];
+	characterMatched = 0;
+	for (j = 0; j < outputLen - keyWordLen; j++)
+	{
+	  strncopy(croppedWord, convertedOutput + j, keyWordLen);
+	  
+	  for (k = 0; k < keyWordLen; k++)
+	  {
+		if (croppedWord[k] == dictionaryWord[k])
+		{
+		  characterMatched++;
+		}
+		if (keyWordLen > 2 && (keyWordLen - characterMatched) < 2)
+		{
+			utstring_renew(newOutput);
+			utstring_printf(newOutput, convertedOutput(0, j));
+			utstring_printf(newOutput, croppedWord);
+			utstring_printf(newOutput, convertedOutput(j + keyWordLen));
+			utstring_renew(output);
+			utstring_printf(output, newOutput);
+			j += keyWordLen - 1;
+			break;
+		}
+	  }
+	}
+  }
+}
+
+void populateDictionary(void)
+{
+  char words[32][15];
+  int i = 0;
+  
+  /*initialize string profile*/
+  utarray_new(dictionary, &ut_str_icd);
+  
+  /*all ansi c keyword */	
+  words[0] = "auto";
+  words[1] = "break";
+  words[2] = "case";
+  words[3] = "char";
+  words[4] = "const";
+  words[5] = "continue";
+  words[6] = "default";
+  words[7] = "do";
+  words[8] = "double";
+  words[9] = "else";
+  words[10] = "enum";
+  words[11] = "extern";
+  words[12] = "float";
+  words[13] = "for";
+  words[14] = "goto";
+  words[15] = "if";
+  words[16] = "int";
+  words[17] = "long";
+  words[18] = "register";
+  words[19] = "return";
+  words[20] = "short";
+  words[21] = "signed";
+  words[22] = "sizeof";
+  words[23] = "static";
+  words[24] = "struct";
+  words[25] = "switch";
+  words[26] = "typedef";
+  words[27] = "union";
+  words[28] = "unsigned";
+  words[29] = "void";
+  words[30] = "volatile";
+  words[31] = "while";
+
+ for (i = 0; i < 32; i++)
+ {
+   utarray_push_back(dictionary, &words[i]);
+ }
 }
