bfbdfecbf1ddbd9c3038df5592a524d44f7abc88
ericr2 <ericr2@uci.edu>
2014-03-04 16:08:01 -0800

    Commented stuff out

--- src/Model/TestIdentifyCharacter.c
+++ src/Model/TestIdentifyCharacter.c
@@ -7,17 +7,14 @@ int main(void)
 {
   UT_array * CharProbabilities;
   
-  CharProbability * p =  utarray_front(CharProbabilities);
+  CharProbability * p;
   
   IMAGE *img = NULL;
   int i;
-  int j;
   IMAGE *image = NULL;
   ILIST *imglist = NULL;
   IENTRY *curr;
   char fname[50] = "CourierNew12_300DPI";
-  char index[20];
-  char sname[50];
 
   i=0;
 
@@ -25,14 +22,15 @@ int main(void)
   img = Crop(image,50,200,1400,500);
   imglist = IsolateCharacter(img, 12, 300);
   curr = imglist->First;
-  j = SaveImage("meow", img);
-  while (curr)
+
+  /*while (curr)
   { 
-	/*CharProbabilities = IdentifyCharacter(curr->Image, imglist);*/
-	 for(p=(CharProbability*) utarray_front(CharProbabilities); p!=NULL; p=(CharProbability*)utarray_next(CharProbability,p)) {
-   	printf("Chance of %c is %d percent\n", p->Char, p->Probability);
-  }
+	CharProbabilities = IdentifyCharacter(curr->Image, imglist);
+	 for( p = (CharProbability*)utarray_front(CharProbabilities); p != NULL; p = (CharProbability*)utarray_next(CharProbability,p)) 
+		{
+   			printf("Chance of %c is %d percent\n", p->Char, p->Probability);
+  		}
 	curr = curr->Next;
-  }
+  }   */
   return 0;
 }
