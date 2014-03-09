33ec5c86e8b384286582b3ca6608d3759d58082b
mqchau <mqchau@uci.edu>
2014-03-09 14:57:23 -0700

    update TestCompile

--- test/TestCompile.c
+++ test/TestCompile.c
@@ -7,9 +7,21 @@ int main(void){
 	
 	UT_string * Carrier;
 	utstring_new(Carrier);
+	
+	utstring_clear(Carrier);
 	utstring_printf(Carrier, "%s", TestString);
 	UT_string * Receiver = GetCompileMessage(Carrier);
 	printf("Compile message is:\n%s\n", utstring_body(Receiver));
+	utstring_free(Receiver);
+	
+	char TestString2[] = "#include <stdlib.h>\n#include <stdio.h>\nint main(void){\nprintf(\"Hello World.\\n\")\nreturn 0;\n}";
+	utstring_clear(Carrier);
+	utstring_printf(Carrier, "%s", TestString2);
+	Receiver = GetCompileMessage(Carrier);
+	printf("Compile message is:\n%s\n", utstring_body(Receiver));
+	
+	utstring_free(Carrier);
+	utstring_free(Receiver);
 	return 0;
 	
 }
